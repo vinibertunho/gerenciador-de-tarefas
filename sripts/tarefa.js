4085b6f7501d9236415b0c5985ebe7673a1e504f
ViniBertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 09:48:44 -0300

    feat: Transforma tarefas

--- sripts/tarefa.js
+++ sripts/tarefa.js
@@ -41,12 +41,11 @@ console.table(tarefas);
 
 // Verificando se a tarefa "Ir à academia" está na lista de tarefas
 if (tarefas.includes("Ir à academia")) {
-  console.log('existe a tarefa Ir à academia');
+  console.log("existe a tarefa Ir à academia");
 }
 
 // Transformando o array de tarefas em uma única string, separando cada tarefa por vírgula
-console.log(`Tarefas como string: ${tarefas.join(', ')}`);
-
+console.log(`Tarefas como string: ${tarefas.join(", ")}`);
 
 // Extraindo tarefas dos índices 2 ao 4 (índice inicial incluso, índice final exclusivo)
 let tarefasExtraidas = tarefas.slice(2, 5);
@@ -54,3 +53,25 @@ let tarefasExtraidas = tarefas.slice(2, 5);
 // Exibe as tarefas extraídas
 console.log(tarefasExtraidas);
 // Saída esperada: ['Lavar a louça', 'Comprar mercado', 'Ir à academia']
+
+//removendo atarefa e adiconando outra tarefa
+
+tarefas.splice(3, 1);
+tarefas.splice(3, 0, "Fazer as marmitas da semana");
+
+console.log(tarefas);
+console.table(tarefas);
+
+let tarefas2 = [
+  "Ir para o muay thai",
+  "Ir para o jiujitsu",
+  "Dar banho nos cachorros",
+  "Dar comida para os cachorros"
+];
+
+//adicionando uma palavra no array
+console.log(`Tarefas combinada: ${tarefas.concat(tarefas2).join(', ')}`);
+
+let tarefasComPrefixo = tarefas.map(tarefa => `Fazer: ${tarefa}`);
+
+console.log(tarefasComPrefixo);
