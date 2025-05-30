42b86cb444ccb6b57f00cd8da69e4a37e2767170
ViniBertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 09:03:09 -0300

    feat: Adiciona novas tarefas

---
+++ sripts/tarefa.js
@@ -0,0 +1,19 @@
+let tarefas = [
+  "Estudar JavaScript",
+  "Fazer exercícios de matemática",
+  "Lavar a louça",
+  "Comprar mercado",
+  "Ir à academia",
+  "Ler um livro",
+  "Fazer revisão para o teste",
+  "Limpar o quarto",
+  "Estudar para concurso",
+  "Organizar o computador",
+];
+console.log(tarefas);
+
+tarefas.push("Estudar Ingles", "Comer Fruta");
+
+console.log(tarefas);
+
+console.table(tarefas);
